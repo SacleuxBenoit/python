f970299b97c48bb039035042db00c103eb22006a
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-03-18 20:36:27 +0100

    update exercice 12.1 && 12.2 for :arrow_right: exercice-12-classes.py

--- exercicePython/exercice-12-classes.py
+++ exercicePython/exercice-12-classes.py
@@ -13,10 +13,10 @@
 # réponse 12.1
 class User:
     def __init__(self, firstname, lastname, email, newsletter):
-        firstname = ""
-        lastname = ""
-        email = ""
-        newsletter = False
+        self.firstname = ""
+        self.lastname = ""
+        self.email = ""
+        self.newsletter = False
 
 
 # exo 12.2
@@ -43,13 +43,18 @@ class User:
 #   - newsletter: true
 
 # réponse 12.2
-
+user1 = User("Joe","Dalton","Joe.dalton@example.com",True)
+user2 = User("William","Dalton","william.dalton@example.com",False)
+user3 = User("Jack","Dalton","jack.dalton@example.com",False)
+user4 = User("Avrel", "Dalton", "avrel.dalton@example.com",True)
 # exo 12.3
 # Ajoutez chacune des instances de la classe `User` à une liste nommée `users`
 # Utilisez une boucle `for` (type `foreach`) pour afficher le nom complet et l'email de chaque utilisateur s'il est abonné à la newsletter (c-à-d si newsletter == True)
 
 # réponse 12.3
-
+users = [user1,user2,user3,user4]
+for user in users:
+    print(user.firstname)
 # exo 12.4
 # Créez une classe nommée `ProductLorem` qui possède les attributs suivants :
 # - _name: valeur par défaut ''
