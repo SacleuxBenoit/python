f641072811aa0db35ff9b5bc9cc24c6b995c7cd4
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-04-11 17:42:01 +0200

    update exercice 12.1, 12.2, 12.3, 12.4

--- exercicePython/exercice-12-classes.py
+++ exercicePython/exercice-12-classes.py
@@ -12,11 +12,11 @@
 
 # réponse 12.1
 class User:
-    def __init__(self, firstname, lastname, email, newsletter):
-        self.firstname = ""
-        self.lastname = ""
-        self.email = ""
-        self.newsletter = False
+    def __init__(self, firstname: str = "", lastname: str = "", email: str = "", newsletter: bool = False):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.newsletter = newsletter
 
 
 # exo 12.2
@@ -47,14 +47,18 @@ user1 = User("Joe","Dalton","Joe.dalton@example.com",True)
 user2 = User("William","Dalton","william.dalton@example.com",False)
 user3 = User("Jack","Dalton","jack.dalton@example.com",False)
 user4 = User("Avrel", "Dalton", "avrel.dalton@example.com",True)
+
 # exo 12.3
 # Ajoutez chacune des instances de la classe `User` à une liste nommée `users`
 # Utilisez une boucle `for` (type `foreach`) pour afficher le nom complet et l'email de chaque utilisateur s'il est abonné à la newsletter (c-à-d si newsletter == True)
 
 # réponse 12.3
 users = [user1,user2,user3,user4]
+
 for user in users:
-    print(user.firstname)
+    if(user.newsletter):
+        print(user.firstname, user.lastname, user.email)
+        
 # exo 12.4
 # Créez une classe nommée `ProductLorem` qui possède les attributs suivants :
 # - _name: valeur par défaut ''
@@ -67,7 +71,16 @@ for user in users:
 # - set_price() : détermine le prix du produit
 
 # réponse 12.4
-
+class ProductLorem:
+    def __init__(self, name: str = "", price:float = 0.0):
+        self.name = name
+        self.price = price
+        
+    def get_name(self) -> str:
+        return self.name
+    
+    def get_price(self) -> float:
+        return self.price
 # exo 12.5
 # Créez 3 instances de la classe `ProductLorem` et affectez les valeurs suivantes à ses attributs en utilisant les setters :
 # - product1
